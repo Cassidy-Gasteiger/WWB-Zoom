0b4189d951d678816c56b13592a7d4efbc373ae0
Milind Basavaraja <milindbasavaraja96@gmail.com>
2023-07-16 06:31:05 -0400

    WWB-V1

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './buttons.css';
-import Popup from "./popup"; // Import the CSS file
-import { TileLayout } from "@progress/kendo-react-layout";
+import Popup from "./popup";
+import LoadingSpinner from "./Spinner";
+import EventCalender from "./calender";
 
 
 const ButtonStack = () => {
@@ -9,25 +10,28 @@ const ButtonStack = () => {
 
     const [showPopup, setShowPopup] = useState(false);
     const [popupMessage, setPopupMessage] = useState(null);
-    //const [loading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(false);
+    const [showAnotherComponent, setShowAnotherComponent] = useState(false);
     const handleConnection = async () => {
         try {
-            //setLoading(true)
+            setLoading(true)
             const response = await fetch('http://localhost:8000/api/connection', {headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },}); // Update the API endpoint as needed
             const data = await response.text();
             setPopupMessage(data);
-            //setLoading(false);
             setShowPopup(true);
+            setLoading(false);
         } catch (error) {
             console.error('Error fetching data:', error);
+            setLoading(false);
         }
     };
 
     const handleAudio = async () => {
         try {
+            setLoading(true)
             const response = await fetch('http://localhost:8000/api/microphone', {headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
@@ -35,8 +39,10 @@ const ButtonStack = () => {
             const data = await response.text();
             setPopupMessage(data);
             setShowPopup(true);
+            setLoading(false);
         } catch (error) {
             console.error('Error fetching data:', error);
+            setLoading(false);
         }
     };
 
@@ -58,9 +64,20 @@ const ButtonStack = () => {
         setShowPopup(false);
     };
 
+    const handleClick = () => {
+        /*const newWindow = window.open('', '_blank'); // Open a new window or tab
+        newWindow.document.write('<html><body><div id="root"></div></body></html>'); // Create a basic HTML structure in the new window
+        ReactDOM.render(<EventCalender />, newWindow.document.getElementById('root'));*/
+        setShowAnotherComponent(true); // Set the state to true when the button is clicked
+    };
+
 
 
     return (
+        <>
+            <center>
+        {isLoading && <LoadingSpinner/>}
+            </center>
         <div className="center-container">
             <div className="button-container">
       <button onClick={handleConnection} className="button">Bandwidth</button>
@@ -69,8 +86,11 @@ const ButtonStack = () => {
         {showPopup && <Popup message={popupMessage} onClose={closePopup} />}
       <button onClick={handleVideo} className="button">Video</button>
         {showPopup && <Popup message={popupMessage} onClose={closePopup} />}
+        <button onClick={handleClick} className="button">Add the event to the Calender</button>
+        {showAnotherComponent && <EventCalender />}
         </div>
         </div>
+        </>
   );
 };
 
---
+++ src/components/calender.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Calendar from "@ericz1803/react-google-calendar";
+import { css } from "@emotion/react";
+import "./popup.css"
+//put your google calendar api key here
+const API_KEY = "AIzaSyC8t8iPMotcIDR67kuRX9OqHphhKRQacmg";
+
+//replace calendar id with one you want to test
+
+let calendars = [
+    { calendarId: "09opmkrjova8h5k5k46fedmo88@group.calendar.google.com" }
+];
+
+let styles = {
+    //you can use object styles
+    calendar: {
+        borderWidth: "3px" //make outer edge of calendar thicker
+    },
+
+    //you can also use emotion's string styles (remember to add the line 'import { css } from "@emotion/react";')
+    today: css`
+    /* highlight today by making the text red and giving it a red border */
+    color: red;
+    border: 1px solid red;
+  `
+};
+
+
+
+export default function EventCalender() {
+    return (
+        <div className="popup-overlay">
+            <div className="popup-content">
+            <div
+                /*style={{
+                    width: "50%",
+                    paddingTop: "50px",
+                    paddingBottom: "50px",
+                    maxWidth: "1200px",
+                    marginTop: "300px",
+                    marginLeft: "300px"
+                }}*/
+            >
+                <Calendar apiKey={API_KEY} calendars={calendars} styles={styles} />
+            </div>
+       </div>
+        </div>
+    );
+}
